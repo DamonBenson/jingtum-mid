eb1f9828054f830fdf7ded2d0c5a70f6088ea825
qiumufei <qiumufei@bupt.edu.cn>
2021-07-07 15:45:31 +0800

    ipfs upload file.

--- main/mid/processFunction/authMid.js
+++ main/mid/processFunction/authMid.js
@@ -1,12 +1,17 @@
+import fs from 'fs';
+import crypto from 'crypto-js';
 import sha256 from 'crypto-js/sha256.js';
 import mysql from 'mysql';
 import sqlText from 'node-transform-mysql';
+import moment from 'moment';
 
 import * as requestInfo from '../../../utils/jingtum/requestInfo.js';
 import * as erc721 from '../../../utils/jingtum/erc721.js';
 import * as localUtils from '../../../utils/localUtils.js';
 import * as mysqlUtils from '../../../utils/mysqlUtils.js';
+import * as ipfsUtils from '../../../utils/ipfsUtils.js';
 import * as authValidate from '../../../utils/validateUtils/auth.js';
+import * as httpUtils from '../../../utils/httpUtils.js';
 
 import {userAccount} from '../../../utils/config/jingtum.js';
 import {mysqlConf} from '../../../utils/config/mysql.js';
@@ -302,3 +307,212 @@ export async function handleInnerWorkAuth(tokenRemote, seqObj, req) {
 //     return resInfo;
 
 // }
+
+/**
+ * @description 不通过合约完成同步作品确权。
+ * @param {int}workId 作品标识
+ * @param {String}address 确权用户地址
+ */
+export async function handleWorkAuth(tokenRemote, seqObj, req) {
+
+    console.time('handleWorkAuth');
+
+    let resInfo = {
+        msg: 'success',
+        code: 0,
+        data: {},
+    }
+
+    let body = req.body;
+    console.log(body);
+    try {
+        await authValidate.innerWorkAuthReqSchema.validateAsync(body);
+    } catch(e) {
+        e.details.map((detail, index) => {
+            console.log('error message ' + index + ':', detail.message);
+        });
+        resInfo.msg = 'invalid parameters',
+        resInfo.code = 1;
+        resInfo.data.validateInfo = e;
+        console.log('/auth/work:', resInfo.data);
+        console.timeEnd('handleWorkAuth');
+        console.log('--------------------');
+        return resInfo;
+    }
+
+    // 方法体
+    let workId = body.workId;
+    let address = body.address;
+
+    let basePath = "";
+
+    let package1 = await genPackage1(workId, address);
+    let package1Path = basePath + "/authFiles/package/" + package1.params.package_token + ".json";
+    let package1Hash = localUtils.saveJson(package1, package1Path);
+
+    let express1 = genExpress1(package1, package1Hash);
+    let express1Path = basePath + "/authFiles/express/" + package1.params.package_token + ".json";
+    let express1Hash = localUtils.saveJson(express1, express1Path);
+
+    let detSn = await httpUtils.postFiles("http://117.107.213.242:8888/spaceDET/uploadDET", {files: [package1Path, express1Path]});
+
+    await localUtils.sleep(5000);
+
+    let checkRes = await httpUtils.post("http://117.107.213.242:8888/check/checkKeyTostorage", {det_sn: detSn});
+
+    let fileHashArr = getNeedUpload(checkRes);
+
+    for(let i in fileHashArr) {
+        let uploadFileInfo = {
+            files: fileHashArr[i],
+            det_sn: detSn,
+            packageHash: package1Hash,
+            is_split: 0
+        }
+        let uploadRes = await httpUtils.post("http://117.107.213.242:8888/spaceUpload/uploadDETFile", uploadFileInfo);
+    }
+
+    
+    console.log('/auth/work:', resInfo);
+
+    console.timeEnd('handleWorkAuth');
+    console.log('--------------------');
+
+    return resInfo;
+
+}
+
+async function genPackage1(workId, address) {
+
+    let name = workId + address;
+
+    let copyrightFilter = {
+        work_id: body.workId,
+        address: body.address,
+    }
+
+    let sql = sqlText.table('work_info').where(copyrightFilter).select();
+    let workInfo = await mysqlUtils.sql(c, sql)[0];
+    let fileInfo = workInfo.file_info_list[0];
+    let workPath = fileInfo.fileHash;
+
+    let localWorkPath = basePath + "/authFiles/work/" + workPath;
+    await ipfsUtils.getFile(workPath, localWorkPath);
+
+    let fd = fs.createReadStream(localWorkPath);
+    let hash = crypto.createHash('sha256');
+    hash.setEncoding('hex');
+
+    fd.on('end', function() {
+        
+        hash.end();
+        let workHash = hash.read();
+
+        let package1 = {
+            "cover": cover,
+            "cover_hash": coverHash,
+            "name": name,
+            "subject": [
+                {
+                    "name": subjectName,
+                    "type": subjectType,
+                    "usn": subjectUsn
+                }
+            ],
+            "object": [
+                {
+                    "is_split": 0,
+                    "split_num": "",
+                    "works_hash": workHash,
+                    "works_name": workName,
+                    "works_path": workPath,
+                    "works_size": 0,
+                    "works_type": workType
+                }
+            ],
+            "copyright_rights_get": 0,
+            "params": {
+                "batch_name": name,
+                "det_business_code": "C001_01_01",
+                "package_token": packageToken,
+                "step": 1,
+                "submit_usn": subjectUsn,
+                "works_count": 1
+            }
+        }
+    
+        return package1;
+
+    });
+
+    fd.pipe(hash);
+
+    let workName = workInfo.work_name;
+    let workType = fileInfo.work_type;
+
+    let cover = base + "/resource/test.jpg";
+    let coverHash = "017ec8060ae3cd8d7419b73f4f0bf77a7b963dd41a7af2deda1b4bf556835099";
+    let subjectName = "";
+    let subjectType = "";
+    let subjectUsn = "";
+    let packageToken = sha256(subjectUsn + moment().unix() + localUtils.randomNumber(0,9999)).toString();
+
+}
+
+function genExpress1(package1, package1Hash) {
+
+    let detPackageName = package1.name;
+    let detTime = moment().format('YYYY-MM-DD');
+    let fromSpaceUser = package1.subject[0].usn;
+    let fileHash1 = package1.object[0].workHash;
+    let fileName1 = package1.object[0].workName;
+    let filePath1 = package1.object[0].workPath;
+    let fileSize1 = package1.object[0].workSize;
+    let packageToken = package1.params.package_token;
+
+    let express1 = {
+        "det_business_code": "C001_01_01",
+        "det_package_name": detPackageName,
+        "det_package_num": 1,
+        "det_time": detTime,
+        "from_space_address": "",
+        "from_space_device": "",
+        "from_space_ip": "",
+        "from_space_user": fromSpaceUser,
+        "package_list": [
+            {
+                "file_list": [
+                    {
+                        "file_hash": fileHash1,
+                        "file_name": fileName1,
+                        "file_path": filePath1,
+                        "file_size": fileSize1,
+                        "is_split": 0,
+                        "split_num": ""
+                    }
+                ],
+                "package_hash": package1Hash,
+                "package_name": detPackageName,
+                "package_token": packageToken
+            }
+        ],
+        "to_space_address": "",
+        "to_space_user": ""
+    }
+      
+    return express1;
+
+}
+
+function getNeedUpload(checkRes) {
+
+    let fileHashArr = checkRes.data.package_list.file_list.map(fileInfo => {
+        return {
+            fileHash: fileInfo.file_hash,
+            fileStatus: fileInfo.file_status,
+        }
+    }).filter(fileInfo => !fileInfo.fileStatus);
+
+    return fileHashArr;
+
+}
--- utils/httpUtils.js
+++ utils/httpUtils.js
@@ -1,5 +1,7 @@
 import http from 'http';
+import fs from 'fs';
 import querystring from 'querystring';
+import formData from 'form-data';
 
 export function get(url, data) {
 
@@ -54,6 +56,60 @@ export function post(url, data) {
         headers: {
             "Content-Type": "application/json"
         },
+    };
+
+    return new Promise((resolve, reject) => {
+
+        let req = http.request(options, res => {
+
+            res.setEncoding('utf8');
+            let data = '';
+    
+            res.on('data', chunk => {
+                data += chunk;
+            });
+    
+            res.on('end', () => {
+                let parsedData = JSON.parse(data);
+                resolve(parsedData);
+            });
+        
+        });
+
+        req.on('error', e => {
+            reject(e.message);
+        });
+
+        req.write(data);
+        req.end();        
+
+    });
+
+}
+
+export function postFiles(url, fileInfo) {
+
+    url = new URL(url);
+
+    let form = new formData();
+    for(let key in fileInfo) {
+        if(Array.isArray(fileInfo[key])) {
+            for(let i in fileInfo[key]) {
+                form.append(key, fs.createReadStream(fileInfo[key][i]));
+            }
+        }
+        else {
+            form.append(key, fileInfo[key]);
+        }
+    }
+    let headers = form.getHeaders();
+
+    let options = {
+        host: url.hostname,
+        port: url.port,
+        path: url.pathname,
+        method: 'POST',
+        headers: headers,
     };
 
     return new Promise((resolve, reject) => {
