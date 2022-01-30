09dd9c711aebf97a6712c48d2b96d5783752264c
DamonBenson <a1696179007@163.com>
2022-01-30 08:36:43 +0800

    补回“设置发行权限”至ERC721

--- utils/jingtum/erc721.js
+++ utils/jingtum/erc721.js
@@ -7,6 +7,58 @@ const issuerAddr = tokenChain.account.issuer.address;
 const issuerSecr = tokenChain.account.issuer.secret;
 
 /**
+ * @description 设置发行权限。
+ * @param {Object}remote 底层链连接对象
+ * @param {String}publisher 被授权发行的地址
+ * @param {int}seq 动态发币账号的交易序列号
+ * @param {String}name 被授权发行的通证名称
+ * @param {int}num 被授权发行的通证数量
+ * @param {int}flag 被授权发行的通证冻结标识
+ * @param {String[]}flagAddrs 拥有修改flag权限的地址数组
+ * @param {String[]}tokenInfosAddrs 拥有修改tokenInfos权限的地址数组
+ * @param {int}refFlag 0-一般通证（版权权利通证）；1-有引用关系的通证（版权许可通证）
+ * @param {bool}showRes 是否显示结果
+ * @returns {Object} 交易处理结果，具体格式见jingtum-lib文档
+ */
+ export function buildTokenIssueTx(remote, publisher, seq, name, num, flag, flagAddrs, tokenInfosAddrs, refFlag, showRes) {
+
+    let tx = remote.buildTokenIssueTx({
+        account: issuerAddr,
+        publisher: publisher,
+        token: name,
+        number: num,
+        flag: flag,
+        roles: localUtils.toRolesArr(flagAddrs, tokenInfosAddrs),
+        referenceFlag: refFlag,
+    });
+
+    tx.setSecret(issuerSecr);
+
+    if(seq) {
+        tx.setSequence(seq);
+    }
+
+    return new Promise((resolve, reject) => {
+        tx.submit(function(err, result) {
+            if(err) {
+                console.log('err:', err);
+                reject(err);
+            }
+            else if(result){
+                if(showRes) {
+                    console.log('buildTokenIssueTx:', result);
+                }
+                else {
+                    console.log('buildTokenIssueTx:', result.engine_result + "_" + result.tx_json.Sequence);
+                }
+                resolve(result);
+            }
+        });   
+    });
+
+}
+
+/**
  * @description 发行一般通证。
  * @param {Object}remote 底层链连接对象
  * @param {String}publisher 发行通证账号的地址
